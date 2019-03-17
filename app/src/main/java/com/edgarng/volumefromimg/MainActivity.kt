fc0d066bba46e3441df489aec4ca445b235cf8d9
EdgarNg1024 <EdgarNg_1024@163.com>
2019-03-17 22:55:15 +0800

    完成自小外接矩形的描绘
    完成自小外接矩形的描绘

--- app/src/main/java/com/edgarng/volumefromimg/MainActivity.kt
+++ app/src/main/java/com/edgarng/volumefromimg/MainActivity.kt
@@ -10,7 +10,7 @@ import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
-import org.opencv.core.Mat
+import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 
 
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn.setOnClickListener {
-            procSrc2Gray()
+            procSrc2rectangle()
         }
     }
 
@@ -37,6 +37,42 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    fun procSrc2rectangle() {
+        val srcImg = Mat()
+        val srcBitmap = BitmapFactory.decodeResource(resources, R.mipmap.aa)
+        Utils.bitmapToMat(srcBitmap, srcImg)//convert original bitmap to Mat, R G B.
+        val dstImg = srcImg.clone()
+        Imgproc.cvtColor(srcImg, srcImg, Imgproc.COLOR_RGB2GRAY)//rgbMat to gray grayMat
+        Imgproc.threshold(srcImg, srcImg, 100.0, 255.0, Imgproc.THRESH_BINARY)
+
+        var contours = mutableListOf<MatOfPoint>()
+        var hierarcy = Mat()
+
+        Imgproc.findContours(srcImg, contours, hierarcy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE)
+        var boundRect = mutableListOf<Rect>()
+        var box = mutableListOf<RotatedRect>()
+        var rect = arrayOf<Point>(Point(), Point(), Point(), Point())
+
+        for (i in contours.indices) {
+            box.add(Imgproc.minAreaRect(MatOfPoint2f(*contours[i].toArray())))
+            boundRect.add(Imgproc.boundingRect(contours[i]))
+            Imgproc.circle(dstImg, Point(box[i].center.x, box[i].center.y), 5, Scalar(0.0, 255.0, 0.0), -1, 8)
+            box[i].points(rect)
+            Imgproc.rectangle(
+                dstImg, Point(boundRect[i].x.toDouble(), boundRect[i].y.toDouble()),
+                Point(boundRect[i].x + boundRect[i].width.toDouble(), boundRect[i].y + boundRect[i].height.toDouble()),
+                Scalar(0.0, 255.0, 0.0), 2, 8
+            )
+            for (j in 0..3) {
+                Imgproc.line(dstImg, rect[j], rect[(j + 1) % 4], Scalar(0.0, 0.0, 255.0), 2, 8)
+            }
+        }
+        Utils.matToBitmap(dstImg, srcBitmap) //convert mat to bitmap
+        img.setImageBitmap(srcBitmap)
+
+    }
+
 
     fun procSrc2Gray() {
         val rgbMat = Mat()
